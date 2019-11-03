e7f02cc5e1daa14256150487dbcdb8bf0a4d293f
Takahito Yamada <d_p.t.a.d.fighter@icloud.com>
2019-11-03 22:44:34 +0900

    finish_3

--- 3/Sample2.hs
+++ 3/Sample2.hs
@@ -36,5 +36,77 @@ deepgin' s = s
 
 safeSqrt::(Ord a, Floating a) => a -> Maybe a
 safeSqrt x
- | x < 0 = Nothing
- | otherwise = Just (sqrt x)
+| x < 0 = Nothing
+| otherwise = Just (sqrt x)
+
+caseOfFirstLetter::String -> String
+-- partern match
+caseOfFirstLetter "" = "empty"
+-- guard
+caseOfFirstLetter(x:xs)
+| 'a' <= x && x <= 'z' = "lower"
+| 'A' <= x && x <= 'Z' = "upper"
+| otherwise = "other"
+
+caseOfFirstLetter'::String -> String
+caseOfFirstLetter' str = 
+case str of
+"" -> "empty"
+(x:xs) -> if 'a' <= x && x <= 'z'
+then "lower"
+else if 'A' <= x && x <= 'Z'
+then "upper"
+else "other"
+
+caseOfFirstLetter''::String -> String
+caseOfFirstLetter'' "" = "empty"
+caseOfFirstLetter''(x:xs)
+| inRange 'a' 'z' = "lowwer"
+| inRange 'A' 'Z' = "upper"
+| otherwise = "other"
+where
+inRange lower upper = lower <= x && x <= upper
+
+
+length'::[a] -> Int
+length' [] = 0
+length'(x:xs) = 1 + length' xs
+
+take'::Int -> [a] -> [a]
+take' n _ | n <= 0 = [] -- guard
+take' _ [] = []
+take' n (x:xs) = x : take' (n-1) xs
+
+drop':: Int -> [a] -> [a]
+drop' n xs | n <= 0 = xs
+drop' _ [] = []
+drop' n (_:xs) = drop' (n-1) xs 
+
+-- sort
+ins::Ord a => a -> [a] -> [a]
+ins e [] = [e]
+ins e (x:xs)
+| e < x = e : x : xs
+| otherwise = x : ins e xs
+
+insSort::Ord a => [a] -> [a]
+insSort [] = []
+insSort (x:xs) = ins x (insSort xs)
+
+each::(a -> b) -> (c -> d) -> (a, c) -> (b, d)
+each f g (x, y) = (f x, g y)
+
+filter'::(a->Bool) -> [a] -> [a]
+filter' p [] = []
+filter' p (x:xs)
+| p x = x : filter' p xs
+| otherwise = filter' p xs
+
+map':: (a -> b) -> [a] -> [b]
+map' _ [] = []
+map' f (x:xs) = f x : map' f xs
+
+zipWith' :: (a -> b -> c) -> [a] -> [b] -> [c]
+zipWith' f [] _ = []
+zipWith' f _ [] = []
+zipWith' f (x:xs) (y:ys) = f x  y : f xs ys
